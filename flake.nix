8ec28322d24ec741a0a98db028839e0109933e50
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-14 15:33:47 +0200

    workaround(nix): define superbuild shell manually

--- flake.nix
+++ flake.nix
@@ -29,44 +29,68 @@
                 "rosidl-typesupport-c"
                 "rosidl-typesupport-cpp"
                 "ament-cmake"
+                "mc-rtc-magnum"
               ];
               extraDevPackages = [ "pkg-config" ];
               overrideAttrs.mc-panda =
-                _:
-                (_super: {
+              { drv-final, drv-prev, ... }:
+              {
                   src = lib.cleanSource /home/arnaud/devel/mc-rtc-nix/workspace/mc_panda;
-                  # cmakeFlags = super.cmakeFlags ++ [
+                  # cmakeFlags = drv-prev.cmakeFlags ++ [
                   #   "-DPYTHON_BINDINGS=OFF"
                   # ];
-                });
+              };
               overrideAttrs.mc-panda-lirmm =
-                _:
-                (_super: {
+              { drv-final, drv-prev, ...}:
+              {
                   src = lib.cleanSource /home/arnaud/devel/mc-rtc-nix/workspace/mc_panda_lirmm;
-                });
+              };
               overrideAttrs.panda-prosthesis =
-                _:
-                (_super: {
+              { drv-final, drv-prev, ...}:
+              {
                   src = lib.cleanSource ./.;
-                });
+              };
               overrideAttrs.mc-rtc-superbuild =
-                final:
-                (super: {
+              { pkgs-final, ...}:
+              {
+                  pkg-name = "panda-prosthesis-superbuild-flake-override";
                   robots = [
-                    final.panda-prosthesis
-                    final.mc-panda-lirmm
-                    final.mc-panda
+                    pkgs-final.panda-prosthesis
+                    pkgs-final.mc-panda-lirmm
+                    pkgs-final.mc-panda
                   ];
-                  controllers = [ final.panda-prosthesis ];
+                  controllers = [ pkgs-final.panda-prosthesis ];
                   # extra mc_rtc.yaml
-                  configs = [ "${final.panda-prosthesis}/lib/mc_controller/etc/mc_rtc.yaml" ];
+                  configs = [ "${pkgs-final.panda-prosthesis}/lib/mc_controller/etc/mc_rtc.yaml" ];
                   observers = [];
-                  plugins = [ final.panda-prosthesis ];
-                  apps = [ final.mc-rtc-magnum final.mc-franka final.mc-rtc-ticker ];
-                });
+                  plugins = [ pkgs-final.panda-prosthesis ];
+                  apps = [ pkgs-final.mc-rtc-magnum pkgs-final.mc-franka pkgs-final.mc-rtc-ticker ];
+              };
             };
           }
         ];
+        perSystem = { system, pkgs, ...}: {
+          # devShells = {inherit (inputs.mc-rtc-nix.devShells.${system}) mc-rtc-superbuild; };
+          devShells.local-superbuild = pkgs.callPackage "${inputs.mc-rtc-nix}/shell.nix" {
+            inherit pkgs;
+            name = "panda-prosthesis-local";
+            # mc-rtc-superbuild = pkgs.mc-rtc-superbuild-base;
+            mc-rtc-superbuild = pkgs.callPackage "${inputs.mc-rtc-nix}/pkgs/mc-rtc/mc-rtc-superbuild-standalone.nix" {
+              pkg-name = "panda-prosthesis-superbuild-flake-override";
+              robots = [
+                pkgs.panda-prosthesis
+                pkgs.mc-panda-lirmm
+                pkgs.mc-panda
+              ];
+              controllers = [ pkgs.panda-prosthesis ];
+              # extra mc_rtc.yaml
+              configs = [ "${pkgs.panda-prosthesis}/lib/mc_controller/etc/mc_rtc.yaml" ];
+              observers = [];
+              plugins = [ pkgs.panda-prosthesis ];
+              apps = [ pkgs.mc-rtc-magnum pkgs.mc-franka pkgs.mc-rtc-ticker ];
+            };
+          };
+        };
       }
     );
 }
